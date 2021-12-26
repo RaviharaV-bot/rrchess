a1b454f36c5e18e6a9c7b15024ff34444a0e2cf5
SriMethan <77402282+SriMethan@users.noreply.github.com>
2021-12-26 10:23:48 +0530

    Update compress.py

--- server/compress.py
+++ server/compress.py
@@ -17,7 +17,7 @@ V2C = {
     "kingofthehill": "L",
     "racingkings": "F",
     "antichess": "f",
-    "losers": "N",
+    "loserschess": "N",
     "horde": "H",
     "threecheck": "V",
     "makruk": "m",
